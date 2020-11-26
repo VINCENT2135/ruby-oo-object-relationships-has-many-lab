0f935c45c4c4248ddb6c893fefae147069ad7e30
Vincent Ivos <vincentivos1@gmail.com>
2020-11-25 20:49:28 -0500

    commit tree bfd1c566d2d49b8cfd06135291ccc2b8ea495474

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,22 @@ class Artist
        song.artist == self
     
     end  
+end
+
+def add_song(song)
+    song.artist = self 
+end
+
+def add_song_by_name(song_name)
+    singer = Song.new(song_name)
+    add_song(singer)
+end 
+
+def self.song_count 
+    Song.all.count
+end 
+
 
 end 
-end
 
 
